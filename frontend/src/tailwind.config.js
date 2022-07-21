efaa3ac5471e36b7a12335899e49a632b5000d5c
Marie_Tignol <marie-tignol_student2022@wilder.school>
2022-07-21 10:19:51 +0200

    config the template and add images + dependencies

---
+++ frontend/src/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}}"],
+  darkMode: false,
+  theme: {
+    colors: {
+      dark: "#1D1D1D",
+      lightOrange: "#FFC897",
+      purple: "#D7BCFD",
+      white: "#FFFFFF",
+      middleGray: "#E1E1E1",
+      gray: "#969393",
+      lightGray: "#F7F7F7",
+      magenta: "#FF1ABF",
+      Orange: "#EBB840",
+      pink: "#FF6C92",
+      middleOrange: "#FFC897",
+      red: "#FF5757",
+      blue: "#4A9CCD",
+    },
+    fontFamily: {
+      PlayfairDisplay: ["Playfair Display", "serif"],
+      Raleway: ["Raleway", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
